55dcaacdbe7be6648ee3f67144c0a3e437721f7a
LuluLudmi <gilma.aguada30@gmail.com>
2024-08-15 18:39:45 -0300

    inicio

---
+++ clase02/src/clase2/clase2.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class clase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // int
+        int a=123456;
+        System.out.println(a);
+        
+        
+        // long
+        long b= 23457689L;
+        System.out.println(b);
+        
+        //float
+        
+        
+        float c= -340769889;
+        System.out.println(c);
+        
+        
+        //Char
+        
+        char d= 45;
+        System.out.println(d);
+        
+        // Byte
+        
+        byte e= 75;
+        System.out.println(e);
+        
+         //double
+         
+         double f= -567478789;
+         
+         System.out.println(f);
+         
+        
+        
+        
+        
+        
+                
+    }
+    
+}
