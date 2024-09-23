641527d6f007cd8423a2fcdbeff4f9e95a124a5c
LuluLudmi <gilma.aguada30@gmail.com>
2024-09-23 18:28:39 -0300

    Add files via upload

---
+++ ClaseFor/src/clasefor/ClaseFor.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clasefor;
+
+/**
+ *
+ * @author alumno
+ */
+public class ClaseFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         //System.out.println("**Estructura for**");
+         // el for es cuando sabemos de antemano las repeticiones
+        //imprimir los numeros del 1 al 10 uno debajo del otro
+//        for(int i=1; i<10; i+=2){// para asignar y sumar a la vez 2
+//            System.out.println(i);
+//        }
+//        System.out.println("Fin de la estructura for"); 
+//        
+        //
+       //Imprimir los números del 1 al 10, sin imprimir
+        //números 2, 5 y 9, uno abajo del otro.
+        
+        System.out.println("**Estructura for**");
+         for(int i=1; i<10; i+=2){
+            System.out.println(i);
+        }
+        System.out.println("Fin de la estructura for"); 
+        
+       }
+
+    }
+    
+
---
+++ ClaseFor/src/clasefor/ClasesDeFor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clasefor;
+
+/**
+ *
+ * @author alumno
+ */
+public class ClaseFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        System.out.println("**Estructura for**");
+         for(int i=1; i<10; i+=2){
+            System.out.println(i);
+        }
+        System.out.println("Fin de la estructura for"); 
+        
+       }
+
+    }
+    
+
