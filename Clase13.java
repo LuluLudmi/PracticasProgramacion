46ada16fd72fc9c708fb053b13683b3cfc619713
LuluLudmi <gilma.aguada30@gmail.com>
2024-09-25 18:29:51 -0300

    Add files via upload

---
+++ Clase13.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase13;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Clase13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Imprimir los números del 1 al 10, sin imprimir
+        números 2, 5 y 9, uno abajo del otro.
+         */
+//  
+//        for(int i=1; i<=10; i++)
+//            if (i!=2 && i!=5 && i!=9)
+//         
+//        
+//        System.out.println(i);
+//        
+//        /*
+//        Imprimir los números del 1 al 30 sin imprimir
+//        números entre el 10 y el 20 uno abajo del otro
+//        */
+//         for (int i=1; i<=30;i++){
+//             if(i<=10 || i>=20){
+//             System.out.println(i);
+//             
+//             
+//             
+//           /*
+//        Imprimir los números del 1 al 10 salteando de
+//        a dos, uno abajo del otro.
+//        */
+//           
+//           for(int i=1; i<=10; i+=2){
+//               System.out.println(i);
+//               
+        //Imprimir los números del 10 al 1, uno al lado
+        //del otro.
+//        
+//        for(int i=10; i>=1; i--){
+//            System.out.println(i+ "");
+
+        /* Imprimir la suma de los números impares del
+        1 al 10.
+         
+
+//        int nroImpar = 0;
+//        for (int i = 0; i <= 10; i++) {
+//            if (i % 2 != 0) {
+//                nroImpar += i;
+//            }
+//        }
+//        System.out.println("La suma de todos los numeros impares es:" + nroImpar);
+//        
+
+           /*
+        Una persona desea invertir $1000 en un banco, el
+        cual le otorga un 2% de interés mensual ?Cuál
+        será la cantidad de dinero que esta persona
+        tendrá al cabo de un a?o?
+        En el primer mes tendrá acumulado 1000 $ más
+        20 $ de interés ( 2% de 1000 ). En el segundo
+        mes se le sumará un 2% a la base de 1020 $ del
+        mes anterior y así sucesivamente. 
+
+         */
+//        double dinero = 1000;
+//
+//        for (int i = 1; i <= 12; i++) {
+//            dinero = dinero * 1.02;
+//        }
+//
+//        System.out.println(dinero);
+//        
+//        
+//        System.out.println("\n******************\n");
+//
+//        double capitalInicial = 1000;
+//        double montoAcumulado = capitalInicial;
+//        double interes = 2;
+//        int meses = 12;
+//
+//        for (int i = 1; i <= meses; i++) {
+//            System.out.println("Su capital inicial para este mes es de $" + montoAcumulado);
+//            System.out.println("El interes del mes" + i + "es del %" + interes);
+//            double interesMensual = montoAcumulado / 100 * interes;
+//            System.out.println("Que representa una ganancia de: $" + interesMensual);
+//            montoAcumulado += interesMensual;
+//            System.out.println("\n############################\n");
+//
+//        
+//
+//        System.out.println("Al finalizar el a?o, su capital sera de:$" +
+//                Math.round(montoAcumulado));
+//        
+         /*
+        Crear un programa que ingrese una oración y
+        muestre cuál es el carácter que más se repite.
+        Consideraciones
+        * No debe incluir el espacio en blanco.
+        * La oración a ingresar no debe estar vacía.
+        */
+        for(int i=1; i<=10; i++)
+         if (i!=2 && i!=5 && i!=9)       
+      
+        System.out.println(i);     
+         
+         
+         
+         
+         
+         
+    }
+
+}
+
